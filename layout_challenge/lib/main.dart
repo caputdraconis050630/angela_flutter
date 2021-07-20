dd4ed3c4e71414ad166c113471edc4f25978620c
caputdraconis050630 <caputdraconis@kakao.com>
2021-07-21 07:56:52 +0900

    add layout challenge

---
+++ layout_challenge/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+
+              Container(
+                color: Colors.red,
+                width: 100.0,
+
+              ),
+
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                    Container(
+                      color: Colors.green,
+                      width: 100.0,
+                      height: 100.0,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  color: Colors.blue,
+                  width: 100.0,
+                  margin: EdgeInsets.only(right: 0.0)
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
