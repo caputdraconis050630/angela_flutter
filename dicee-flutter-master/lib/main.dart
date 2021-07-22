fd8e7e7924edf2fa8871543325ca569456710d7e
caputdraconis050630 <caputdraconis@kakao.com>
2021-07-22 18:15:25 +0900

    hoit

---
+++ dicee-flutter-master/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var leftDiceNumber = 1;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                print('Left button got pressed.');
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+                child: Image.asset('images/dice1.png'),
+                onPressed: () {
+                  print('Right button got pressed.');
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+}
